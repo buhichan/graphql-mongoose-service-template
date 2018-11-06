830047512f429d671e6994e402426a10beebddeb
buhi <yinshang1992@gmail.com>
2018-11-06 12:51:16 +0800

    v0.0.26

--- build/views/graphql/make-resolver.d.ts
+++
@@ -1,3 +0,0 @@
-import { TypeMapperContext } from "./make-schema";
-import { IMeta } from "../../models/meta";
-export declare function makeResolver(meta: IMeta, context: TypeMapperContext): (source: any) => Promise<any>;
